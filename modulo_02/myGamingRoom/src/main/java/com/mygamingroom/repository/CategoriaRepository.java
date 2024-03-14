4d1331d8c29342f5f3a24734d9d93d462f69334d
Douglas Lima <douglas.lima.queiroz@gmail.com>
2024-03-14 20:26:26 -0300

    gamming store

---
+++ modulo_02/myGamingRoom/src/main/java/com/mygamingroom/repository/CategoriaRepository.java
@@ -0,0 +1,18 @@
+package com.mygamingroom.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.mygamingroom.model.Categoria;
+
+import jakarta.validation.Valid;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	
+   public List<Categoria> findAllByNomeContainingIgnoreCase(@Valid @Param("nome")String nome);
+
+}
